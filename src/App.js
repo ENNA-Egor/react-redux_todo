0267e95e4316d2432923e479dbe4d603561cb7ba
ENNA-Egor <e.yudakov@gmail.com>
2025-01-30 16:01:37 +0300

    Отрисовка интерфейса

--- src/App.js
+++ src/App.js
@@ -5,21 +5,44 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        <div className="App">
+      <h1>Hello Redux</h1>
+      <Counter />
+      </div>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
+
+ 
 }
+
+const Counter = () => {
+  // const count = useSelector((state) => state);
+  // const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>0</h2>
+      <button onClick>-</button>
+      <button onClick>+</button>
+      <button onClick>reset</button>
+      {/* <button onClick={() => dispatch(decrement)}>-</button>
+      <button onClick={() => dispatch(increment)}>+</button>
+      <button onClick={() => dispatch(reset)}>reset</button> */}
+    </div>
+  );
+};
 
 export default App;
