caf1e6f6e85973abf0de89b3cd424c76512d2d60
ENNA-Egor <e.yudakov@gmail.com>
2025-01-31 21:48:38 +0300

    Наснёь с начала

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -4,23 +4,23 @@ import '../App.css';
 
 
 const TodoList = () => {
-    //   const todos = useSelector((state) => state);
+      const todos = [{id: 1, title: "React", cjmpleted: false}];
     //   const dispatch = useDispatch();
       return (
-        <h3>TodoList</h3>
-    //     <ul>
-    //       {todos.map((todo) => (
-    //         <li key={todo.title}>
-    //           <input
-    //             type="checkbox"
-    //             checked={todo.completed}
-    //             onChange={() => dispatch(toggleTodo(todo.id))}
-    //           />{" "}
-    //           {todo.title}{" "}
-    //           <button onClick={() => dispatch(removeTodo(todo.id))}>delete</button>
-    //         </li>
-    //       ))}
-    //     </ul>
+        // <h3>TodoList</h3>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.title}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                // onChange={() => dispatch(toggleTodo(todo.id))}
+              />{" "}
+              {todo.title}{" "}
+              <button >delete</button>
+            </li>
+          ))}
+        </ul>
       );
     };
 
