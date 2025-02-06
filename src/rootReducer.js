c4876e77878feb7b9e127e871891cf39412ac8e7
ENNA-Egor <e.yudakov@gmail.com>
2025-02-06 08:52:31 +0300

    Добавил фильтры. Далее отрисовка UI и добавление туду селекторов.

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from "redux";
 import {todosReducer} from './Todos/todos-reduser';
 import {counterReducer} from './Counter/counter-reduser';
+import {filters} from './Filters/filters-reducer'
 
 
 
@@ -9,6 +10,7 @@ import {counterReducer} from './Counter/counter-reduser';
 const rootReducer = combineReducers({
   counter: counterReducer,
   todos: todosReducer,
+  filters,
   
 });
 export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
