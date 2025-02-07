91ff5bea9c7a72cd48ad8c4cb0b64bce10abf6cd
ENNA-Egor <e.yudakov@gmail.com>
2025-02-07 09:01:13 +0300

    Настроил фильтры

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,11 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { removeTodo, toggleTodo } from '../Todos/todos-actions';
+import {selectVisibleTodos, selectActiveTodos} from '../Todos/todos-selector';
+import {selectActiveFilter} from '../Filters/filters-selectors';
 import '../App.css';
 
 
 const TodoList = () => { // отрисовываем данные
-      const todos = useSelector((state) => state.todos);
+      const activeFilter = useSelector(selectActiveFilter);
       const dispatch = useDispatch();
+      const todos = useSelector((state) => selectVisibleTodos(state, activeFilter));
       return (
         // <h3>TodoList</h3>
         <ul>
