99d68b58f17b3d48edef2f8ec43a51ac3f1109cb
ENNA-Egor <e.yudakov@gmail.com>
2025-02-11 09:34:57 +0300

    Настроил локалсторадж

--- src/Store/rootReducer.js
+++ src/Store/rootReducer.js
@@ -1,11 +1,11 @@
 import { createStore, combineReducers } from "redux";
 import {todosReducer} from './Todos/todos-reduser';
 import {counterReducer} from './Counter/counter-reduser';
-import {filters} from './Filters/filters-reducer'
+import {filters} from './Filters/filters-reducer';
+import {loadState, saveState} from './Todos/local-storage';
 
-
-
-
+const el = loadState();
+console.log (el)
 
 const rootReducer = combineReducers({
   counter: counterReducer,
@@ -13,7 +13,22 @@ const rootReducer = combineReducers({
   filters,
   
 });
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+export const cofigureStore = () => {
+  const persistedState = loadState();
+  console.log (persistedState)
+  const store = createStore(
+    rootReducer, 
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
+  store.subscribe(()=>{
+    saveState(store.getState());
+  });
+
+   return store ;
+};
 
 
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React  from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
-import  {store}  from "./Store/rootReducer";
+import  {cofigureStore}  from "./Store/rootReducer";
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+
+const store = cofigureStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
